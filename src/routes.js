5d2145c2c0f2e558f48d4a51327e5eeb3a9d8494
Tungsinn <gvnalamas@tutanota.com>
2021-08-06 12:28:44 -0300

    Produtcs & Categories routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const routes = express.Router();
 
+const multer = require('multer');
+const imgConfig = require('./middlewares/imgConfig');
+
 const UserController = require('./controllers/UserController');
+const CategoryController = require('./controllers/CategoryController');
+const ProductController = require('./controllers/ProductController');
 const authentication = require('./middlewares/auth');
 
 routes.get('/', (req, res) => {
@@ -13,5 +18,16 @@ routes.post('/users', UserController.create);
 routes.get('/users', authentication, UserController.read);
 routes.put('/users', authentication, UserController.update);
 routes.delete('/users', authentication, UserController.delete);
+
+routes.post('/categories', authentication, CategoryController.create);
+routes.get('/categories', CategoryController.read);
+routes.put('/categories/:id_category', authentication, CategoryController.update);
+routes.delete('/categories/:id_category', authentication, CategoryController.delete);
+
+// Recebe apenas uma imagem no campo "image"
+routes.post('/products', authentication, multer(imgConfig).single('image'), ProductController.create);
+routes.get('/products', ProductController.read);
+routes.put('/products/:id_product', authentication, multer(imgConfig).single('image'), ProductController.update);
+routes.delete('/products/:id_product', authentication, ProductController.delete);
 
 module.exports = routes;
