5160f9fe1ab2e7ee4350683ac62851d9b76d7b42
Tungsinn <gvnalamas@tutanota.com>
2021-08-06 12:29:55 -0300

    Fixes

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
 const db = require('../config/database');
+
 const User = require('../models/User');
+const Product = require('../models/Product');
+const Category = require('../models/Category');
 
 const connection = new Sequelize(db);
 
@@ -9,5 +12,10 @@ connection.authenticate().
   .catch((error) => console.log(error));
 
 User.init(connection);
+Product.init(connection);
+Category.init(connection);
+
+Category.associate(connection.models);
+Product.associate(connection.models);
 
 module.exports = connection;
