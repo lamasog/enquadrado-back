d60efac82ca252d6c675676d822a24febc926e8f
Tungsinn <gvnalamas@tutanota.com>
2021-08-06 09:43:56 -0300

    Tabelas products e categories

---
+++ src/database/migrations/20210806124004-products.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('products', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+
+      id_category: {
+        type: Sequelize.INTEGER,
+        references: { model: 'categories', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+
+      image_uri: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: new Date()
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('products');
+
+  }
+};
