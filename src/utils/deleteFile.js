90ca496198db1de01ba7f91b4602b302c18ad497
Tungsinn <gvnalamas@tutanota.com>
2021-08-06 12:27:56 -0300

    Delete images from /uploads

---
+++ src/utils/deleteFile.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const fs = require('fs');
+
+module.exports = {
+    deleteFile: (fileName) => {
+        fs.unlink(path.resolve(__dirname, '..', '..', 'uploads', fileName), (err) => {
+            if(err) console.log(err);
+        })
+    }
+}
